581e9756acdb3496c122f1fa2c072eb3724fce05
zg1993 <13122780440@163.com>
2019-04-15 09:07:19 +0800

    sql

---
+++ sql/create_table.sql
@@ -0,0 +1,50 @@
+
+create table if not exists `department`
+       (dept_name varchar(20),
+        building varchar(20),
+	budget numeric(12,2),
+	primary key (dept_name)
+       ) charset=utf8;
+
+create table if not exists `instructor`
+       (ID varchar(5),
+        name varchar(20),
+	dept_name varchar(20),
+	salary numeric(8,2),
+	primary key (ID),
+	foreign key (dept_name) references department(dept_name)
+       ) charset=utf8;
+
+create table if not exists `course`
+       (course_id varchar(7),
+        title varchar(50),
+	dept_name varchar(20),
+	credits numeric(2,0),
+	primary key (course_id),
+	foreign key (dept_name) references department(dept_name)
+       ) charset=utf8;
+
+create table if not exists `section`
+       (course_id varchar(8),
+        sec_id varchar(8),
+	semester varchar(6),
+	year numeric(4,0),
+	building varchar(20),
+	room_number varchar(7),
+	tiem_slot_id varchar(4),
+	primary key (course_id, sec_id, semester, year),
+	foreign key (course_id) references course (course_id)
+       ) charset=utf8;
+
+create table if not exists `teaches`
+       (ID varchar(5),
+        course_id varchar(8),
+	sec_id varchar(8),
+	semester varchar(6),
+	year numeric(4,0),
+	primary key (ID, course_id, sec_id, semester, year),
+	foreign key (course_id, sec_id, semester, year) references section (course_id, sec_id, semester, year),
+	foreign key (ID) references instructor (ID)
+       ) charset=utf8;
+
+
---
+++ sql/dept.sql
@@ -0,0 +1,10 @@
+insert into department
+(dept_name, building, budget)
+values
+('Comp.Sci.', 'Taylor', 100000),
+('Biology', 'Watson', 90000),
+('Elec.Eng.', 'Taylor', 85000),
+('Music', 'Packard', 80000),
+('Finance', 'Painter', 120000),
+('History', 'Painter', 50000),
+('Physics', 'Waston', 70000);
