c61a1765c2973d635234803631af65b57076ef06
zhou.gang <zhou.gang@mgm-iot.com>
2019-04-17 19:21:23 +0800

    takes

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -48,4 +48,24 @@ create table if not exists `teaches`
 	foreign key (ID) references instructor (ID)
        ) charset=utf8;
 
+create table if not exists `student`
+       (ID varchar(5),
+        name varchar(20) not null,
+	dept_name varchar(20),
+	tot_cred numeric(3,0) default 0,
+	primary key (ID),
+	foreign key (dept_name) references department (dept_name)
+       ) charset=utf8;
+       
 
+create table if not exists `takes`
+       (ID varchar(5),
+        course_id varchar(8),
+	sec_id varchar(8),
+	semester varchar(6),
+	year numeric(4,0),
+	grade varchar(4),
+	primary key (ID, course_id, sec_id, semester, year),
+	foreign key (course_id, sec_id, semester, year) references section (course_id, sec_id, semester, year),
+	foreign key (ID) references student (ID)
+       ) charset=utf8;
