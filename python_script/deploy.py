8c782c17bf2342f12175f9c332dfbcb57d1f2dad
zg1993 <13122780440@163.com>
2022-08-04 14:06:18 +0800

    deploy

--- python_script/deploy.py
+++ python_script/deploy.py
@@ -10,8 +10,10 @@ gwtph-test项目标识 yaml文件配置
 import os
 import sys
 import argparse
-from click import command
 import yaml
+import subprocess
+from datetime import datetime
+
 
 BASE_PATH = os.path.dirname(__file__)
 # PACKAGE_NAME = 'gwtph-web'
@@ -47,8 +49,26 @@ def build_package(branch, project_path='', package_name='', build_command='yarn 
     os.system('mv dist ' + package_name)
     return True
 
-def backup_dist():
-    pass
+
+# 最多保存5份
+def backup_dist(server_ip, server_deploy_path, package_dir,package_name, back_nums=5):
+    backup_name = package_name + '_' + datetime.today().strftime('%m%d%H%M')
+    # suf = re.sub(u'([^\u0041-\u007a])', '', file)
+    # name = re.sub(u"([^\u0030-\u0039])", "", file)
+    command_args = ['ssh', 'root@{}'.format(server_ip), 'ls {}'.format(server_deploy_path)]
+    result_ls = subprocess.check_output(command_args, encoding='utf8')
+    file_list = result_ls.split('\n')
+    backup_name_list = filter(lambda i:i.startswith(package_name), file_list)
+    if (len(list(backup_name_list))<5):
+        backup_command = "mv {0} {1}".format(package_dir, backup_name)
+    else:
+        rm_file = sorted(backup_name_list)[0]
+        rm_command = "rm -rf {0}".format(rm_file)
+        # os.system('ssh root@' + server_ip + ' ' + rm_command)
+        print(rm_command)
+    # os.system('ssh root@' + server_ip + ' ' + backup_command)
+    print(backup_command)
+
 
 def ssh_deploy(server_ip='', server_deploy_path='', package_name='', project_path='', **kwargs):
     # backup
@@ -56,16 +76,22 @@ def ssh_deploy(server_ip='', server_deploy_path='', package_name='', project_pat
     package_backup_dir = package_dir + '_backup'
     backup_command = "mv {0} {1}".format(package_dir, package_backup_dir)
     print('executing backup ssh command: ' + backup_command)
-    os.system('ssh root@' + server_ip + ' ' + backup_command)
+    backup_dist(server_ip, server_deploy_path, package_dir,package_name)
+    #os.system('ssh root@' + server_ip + ' ' + backup_command)
+
     # scp translate
     package_dir_local = os.path.join(project_path, package_name)
     scp_command = "scp -r {0} root@{1}:{2}".format(package_dir_local, server_ip,server_deploy_path)
     print('executing scp command: ' + scp_command)
-    os.system(scp_command)
+
+    #os.system(scp_command)
+
     # 删除服务器备份包
     rm_command = "rm -rf {0}".format(package_backup_dir)
     print('executing rm  ssh command: ' + rm_command)
-    os.system('ssh root@' + server_ip + ' ' + rm_command)
+
+    #os.system('ssh root@' + server_ip + ' ' + rm_command)
+
     print('deploy success')
     # delete local dist
     os.system('rm -rf' + ' ' + os.path.join(BASE_PATH, package_dir_local))
@@ -78,7 +104,8 @@ def deploy(project, branch):
     deploy_info = server_dict.get(project)
     if(not test_ssh_connect(**deploy_info)):
         return print('unable to connect {server_ip}'.format(**deploy_info))
-    if(build_package(branch, **deploy_info)):
+    # if(build_package(branch, **deploy_info)):
+    if(1):
         ssh_deploy(**deploy_info)
 
 
